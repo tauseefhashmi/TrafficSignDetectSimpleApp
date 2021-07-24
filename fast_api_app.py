86fb582975f4524928f549602f9104e212e2d9e5
Tauseef Ahmad Hashmi <hashmi.tauseef@yahoo.in>
2021-07-24 18:16:13 +0200

    Add files via upload

---
+++ fast_api_app.py
@@ -0,0 +1,70 @@
+from keras.models import load_model
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image
+import numpy as np
+import shutil
+
+model = load_model('Traffic_classifier.h5')
+app = FastAPI()
+
+
+def detect(img):
+    # detect_list=[]
+    classes = { 1:'Speed limit (20km/h)',
+            2:'Speed limit (30km/h)', 
+            3:'Speed limit (50km/h)', 
+            4:'Speed limit (60km/h)', 
+            5:'Speed limit (70km/h)', 
+            6:'Speed limit (80km/h)', 
+            7:'End of speed limit (80km/h)', 
+            8:'Speed limit (100km/h)', 
+            9:'Speed limit (120km/h)', 
+            10:'No passing', 
+            11:'No passing veh over 3.5 tons', 
+            12:'Right-of-way at intersection', 
+            13:'Priority road', 
+            14:'Yield', 
+            15:'Stop', 
+            16:'No vehicles', 
+            17:'Veh > 3.5 tons prohibited', 
+            18:'No entry', 
+            19:'General caution', 
+            20:'Dangerous curve left', 
+            21:'Dangerous curve right', 
+            22:'Double curve', 
+            23:'Bumpy road', 
+            24:'Slippery road', 
+            25:'Road narrows on the right', 
+            26:'Road work', 
+            27:'Traffic signals', 
+            28:'Pedestrians', 
+            29:'Children crossing', 
+            30:'Bicycles crossing', 
+            31:'Beware of ice/snow',
+            32:'Wild animals crossing', 
+            33:'End speed + passing limits', 
+            34:'Turn right ahead', 
+            35:'Turn left ahead', 
+            36:'Ahead only', 
+            37:'Go straight or right', 
+            38:'Go straight or left', 
+            39:'Keep right', 
+            40:'Keep left', 
+            41:'Roundabout mandatory', 
+            42:'End of no passing', 
+            43:'End no passing veh > 3.5 tons' }
+    image = Image.open((img))
+    image = image.resize((64,64))
+    image = np.expand_dims(image, axis=0)
+    detect_list = np.array(image)
+    pred = model.predict([detect_list])[0]
+    pred = np.argmax(pred)
+    prediction = classes[int(pred)]
+    return prediction
+
+@app.post('/detect_sign')
+def root(file: UploadFile = File(...)):
+    with open(file.filename, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    prediction = detect(img=file.filename)
+    return {"prediction":prediction}
